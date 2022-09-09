b8331a153fce1e3fe1ade4e1f609291d7ab72d27
Theofilos Petsios <theofilos@crashoverride.com>
2022-09-09 09:47:00 +0300

    wezterm defaults

---
+++ wezterm.lua
@@ -0,0 +1,46 @@
+return {
+  hide_tab_bar_if_only_one_tab = true,
+  native_macos_fullscreen_mode = true,
+  scrollback_lines = 100000,
+  bold_brightens_ansi_colors = false,
+  adjust_window_size_when_changing_font_size = false,
+  check_for_updates = false, -- banner is annoying
+  warn_about_missing_glyphs = true,
+
+  colors = {
+    background = "#1f2227",
+    foreground = "#d8d8d8",
+
+    cursor_bg = "#f8f8f0",
+    cursor_fg = "#d8d8d8",
+    cursor_border = "#d5dfa5",
+
+    selection_bg = "#FFD73F",
+    selection_fg = "#263137",
+
+    scrollbar_thumb = "#222222",
+
+    split = "#444444",
+
+    ansi = {
+      "#2e3436", -- black
+      "#ff8878", -- red
+      "#4e9a06", -- green
+      "#edd400", -- yellow
+      "#579ed9", -- blue
+      "#ffb2fe", -- magenta
+      "#a2e1f8", -- cyan
+      "#f1f1f1", -- white
+    },
+    brights = {
+      "#7e706b", -- black
+      "#fe978b", -- red
+      "#84d831", -- green
+      "#fce94f", -- yellow
+      "#62b4f8", -- blue
+      "#ffc6ff", -- magenta
+      "#c0e9f8", -- cyan
+      "#ffffff", -- white
+    },
+  },
+}
